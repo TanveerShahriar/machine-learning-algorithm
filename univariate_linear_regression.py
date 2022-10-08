8d78c028b37e42ee8020b3b2ce9971cb6a2d9d1d
TanveerShahriar <tanveershahriara@gmail.com>
2022-10-09 00:41:04 +0600

    univariate linear regression code updated

--- univariate_linear_regression.py
+++ univariate_linear_regression.py
@@ -1,65 +1,46 @@
-import math, copy
+#Importing modules
 import numpy as np
 
-# Load our data set
+#Function for computing cost
+def compute_cost(x_train, y_train, w, b):
+  m = len(x_train)
+  cost = 0
+  for i in range(m):
+    f_wb = w * x_train[i] + b
+    cost += (f_wb - y_train[i]) ** 2
+  return cost / (2 * m)
+
+#Function for computing gradient
+def compute_gradient(x_train, y_train, w, b):
+  m = len(x_train)
+  dj_dw = 0
+  dj_db = 0
+  for i in range(m):
+    f_wb = w * x_train[i] + b
+    dj_dw += (f_wb - y_train[i]) * x_train[i]
+    dj_db += (f_wb - y_train[i])
+  return dj_dw/m, dj_db/m
+
+#Function for gradient descent
+def gradient_descent(x_train, y_train, w_init, b_init, alpha, num_iters):
+  w = w_init
+  b = b_init
+  for i in range(num_iters):
+    dj_dw, dj_db = compute_gradient(x_train, y_train, w, b)
+    w -= alpha * dj_dw
+    b -= alpha * dj_db
+  return w, b
+
+#Data sets
 x_train = np.array([1.0, 2.0])
-y_train = np.array([300.0, 500.0]) 
+y_train = np.array([300.0, 500.0])
 
-#Function to calculate the cost
-def compute_cost(x, y, w, b):
+#Initial w and b
+w_init = 0
+b_init = 0
 
-    m = x.shape[0] 
-    cost = 0
+#Some values for gradient descents
+alpha = 0.01
+num_iters = 100000
 
-    for i in range(m):
-        f_wb = w * x[i] + b
-        cost = cost + (f_wb - y[i])**2
-    total_cost = 1 / (2 * m) * cost
-
-    return total_cost
-
-#Function to calculate gradient
-def compute_gradient(x, y, w, b): 
-    m = x.shape[0]    
-    dj_dw = 0
-    dj_db = 0
-
-    for i in range(m):  
-        f_wb = w * x[i] + b 
-        dj_dw_i = (f_wb - y[i]) * x[i] 
-        dj_db_i = f_wb - y[i] 
-        dj_db += dj_db_i
-        dj_dw += dj_dw_i 
-    dj_dw = dj_dw / m 
-    dj_db = dj_db / m 
-
-    return dj_dw, dj_db
-
-#Function to calculate gradient descent
-def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function): 
-    w = copy.deepcopy(w_in) # avoid modifying global w_in
-    # An array to store cost J and w's at each iteration primarily for graphing later
-    J_history = []
-    p_history = []
-    b = b_in
-    w = w_in
-
-    for i in range(num_iters):
-        # Calculate the gradient and update the parameters using gradient_function
-        dj_dw, dj_db = gradient_function(x, y, w , b)     
-
-        # Update Parameters using equation (3) above
-        b = b - alpha * dj_db                            
-        w = w - alpha * dj_dw                            
-
-        # Save cost J at each iteration
-        if i<100000:      # prevent resource exhaustion 
-            J_history.append( cost_function(x, y, w , b))
-            p_history.append([w,b])
-        # Print cost every at intervals 10 times or as many iterations if < 10
-        if i% math.ceil(num_iters/10) == 0:
-            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
-                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
-                  f"w: {w: 0.3e}, b:{b: 0.5e}")
-
-    return w, b, J_history, p_history #return w and J,w history for graphing
+print(gradient_descent(x_train, y_train, w_init, b_init, alpha, num_iters))
